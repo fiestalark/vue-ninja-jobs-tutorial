02d66d7f4a3af0babd557d81cbed6faea6b03636
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-19 17:03:12 -0400

    Router forward, back & redirect

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,29 @@
     <router-link :to="{ name: 'about' }">About</router-link> |
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'home' })
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -34,5 +55,11 @@ nav a {
 nav a.router-link-exact-active {
   color: white;
   background: #99d194
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 6px;
 }
 </style>
