91d29339b8967185bf4f9be23432e9a8a8562d0f
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-20 20:03:55 -0400

    Set up fetch requests to json-server

--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -11,12 +11,15 @@
 export default {
     data() {
         return {
-            jobs: [
-                { title: 'Ninja UX Designer', id: 1, details: 'lorem'},
-                { title: 'Ninja Web Developer', id: 2, details: 'lorem'},
-                { title: 'Ninja Vue Developer', id: 3, details: 'lorem'}
-            ]
+            jobs: []
         }
+    },
+    // Popular place to get the data is in mounted life-cycle hook; can use the other hooks too. Which is fastest?
+    mounted() {
+      fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     }
 }
 </script>
